52265e5b0a29c0ca796e67028a05439173f2fea6
fahali <fahim.ali.204@gmail.com>
2020-12-17 15:32:40 -0500

    Added scoring, and the ability to reset the game.
    Setting innerHTML to "decode" HTML entities, this will need to be fixed.
    
    Added a modal dialog at the end of 10 questions.

--- script.js
+++ script.js
@@ -36,22 +36,50 @@ const api = {
 
 class Game {
    constructor() {
-      this.index = 0;
+      this.reset();
    }
+
+   reset = () => {
+      this.index = 0;
+      this.score = 0;
+      this.questions = null;
+   };
 
    currentQAPair = () => this.questions[this.index];
 
    currentQuestion = () => this.currentQAPair().question;
 
+   correctAnswer = () => this.currentQAPair().correct_answer;
+
    currentAnswers = () => {
-      return [this.currentQAPair().correct_answer].concat(
+      // Fisher - Yates shuffle algorithm written in Javascript
+      // https://stackoverflow.com/a/6274381
+      const shuffle = array => {
+         for (let i = array.length - 1; i > 0; i--) {
+            const random = Math.floor(Math.random() * (i + 1));
+            const item = array[i];
+            array[i] = array[random];
+            array[random] = item;
+         }
+         return array;
+      };
+
+      const answers = [this.correctAnswer()].concat(
          this.currentQAPair().incorrect_answers
       );
+
+      return shuffle(answers);
    };
 
    nextQuestion = () => this.index++;
 
+   incrementScore = () => this.score++;
+
+   totalQuestions = () => this.questions.length;
+
    setQuestions = questions => (this.questions = questions);
+
+   isGameOver = () => this.index === this.totalQuestions();
 }
 
 const game = new Game();
@@ -75,19 +103,51 @@ document.body.addEventListener('click', event => {
       api.arguments.amount +
       api.defaults.amount;
 
+   const EMPTY = '';
+
    const question = document.querySelector('.question');
    const answers = document.querySelector('.answers');
 
+   const modal = document.querySelector('.modal');
+   const wintext = document.querySelector('.wintext');
+
+   const resetCard = () => {
+      question.textContent = EMPTY;
+      answers.textContent = EMPTY;
+   };
+
+   const resetGame = () => {
+      game.reset();
+      resetCard();
+      modal.style.display = 'none';
+      wintext.textContent = EMPTY;
+   };
+
    const renderQuestion = () => {
-      question.textContent = game.currentQuestion();
+      resetCard();
+
+      question.innerHTML = game.currentQuestion();
       game.currentAnswers().forEach(answer => {
          const button = document.createElement('button');
-         button.textContent = answer;
+         button.classList.add('answer');
+         button.innerHTML = answer;
          answers.appendChild(button);
       });
    };
 
+   const renderScore = () => {
+      const score = document.querySelector('.score');
+      score.textContent = `SCORE: ${game.score} / ${game.totalQuestions()}`;
+   };
+
+   const render = () => {
+      renderScore();
+      renderQuestion();
+   };
+
    if (target.classList.contains('new-game')) {
+      resetGame();
+
       // console.log(defaultUrl); // TODO - remove console.log
       fetch(defaultUrl)
          .then(response => {
@@ -99,9 +159,28 @@ document.body.addEventListener('click', event => {
             if (data.response_code === api.response_codes.success) {
                game.setQuestions(data.results);
             }
-            renderQuestion();
             // console.log(game.questions); // TODO - remove console.log
+            render();
          })
          .catch(error => console.log(error));
+   }
+
+   if (target.classList.contains('answer')) {
+      const choice = target.textContent;
+      if (choice === game.correctAnswer()) {
+         game.incrementScore();
+      }
+      game.nextQuestion();
+
+      if (game.isGameOver()) {
+         wintext.textContent = `Congratulations, you scored ${
+            game.score
+         } out of ${game.totalQuestions()} questions! Play again?`;
+         modal.style.display = 'flex';
+
+         return;
+      }
+
+      render();
    }
 });
